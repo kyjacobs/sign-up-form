bea88fcc6ee83d6da0d4fdcae932bbf6b7db2200
Kyle Jacobs <kycjacob@gmail.com>
2022-07-02 09:34:59 -0700

    CSS mostly done, layout looks good on macbook, but needs to scale with window size and alternate resolutions

---
+++ app.js
@@ -0,0 +1,9 @@
+function passwordConfirm() {
+    
+    const pwd = document.querySelector('#pwd');
+    const pwd_conf = document.querySelector('#pwd-confirm');
+
+    if(pwd_conf !== pwd) {
+        // add text input under form box
+    }
+}
